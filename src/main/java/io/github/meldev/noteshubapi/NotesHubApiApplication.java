ff69eb0138ba859879614f5ceb5a50a491818e02
Mel Saraiva <melcr306@gmail.com>
2026-02-15 17:16:22 -0300

    chore: initial commit

---
+++ src/main/java/io/github/meldev/noteshubapi/NotesHubApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.meldev.noteshubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesHubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/meldev/noteshubapi/NotesHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.meldev.noteshubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
